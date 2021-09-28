77741777a2f205a7f22d8994d969d2bb2d9d7bde
jahenner <jahenner@gmail.com>
2021-09-28 11:44:17 -0400

    Added tabs and description views

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  MovieGridCell
+//
+//  Created by Alex Henner on 9/28/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
